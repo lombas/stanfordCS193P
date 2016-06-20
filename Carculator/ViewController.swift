636b431c9967ea3578357104286d1171cf6aa10a
Leonardo Lombardi <leonardo.lombardi@tdmac04.local>
2016-06-20 10:49:33 -0300

    Initial Commit

---
+++ Carculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carculator
+//
+//  Created by Leonardo Lombardi on 6/20/16.
+//  Copyright © 2016 Uruzilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
